f6cd20bf281181cfcdca3aa667800ea2e1af2f28
Olga Tkachenko <tkachenko.work@mail.ru>
2020-10-25 19:19:40 +0300

    Ajax

---
+++ ajax/feedback.php
@@ -0,0 +1,17 @@
+﻿<?php 
+	$name = htmlspecialchars($_POST['name']);
+	$emile = htmlspecialchars($_POST['emile']);
+	$subject = htmlspecialchars($_POST['subject']);
+	$message = htmlspecialchars($_POST['message']);
+	if ($name == ''|| $emile == '' || $subject == ''|| $message == '') {
+		echo "Заполните все поля";
+		exit;
+	}
+	//Отправка
+	$subject = "=?utf-8?B?".base64_encode($subject)."?=";
+	$headers = "From: $emile\r\nReply-to: $emile\r\nContent-type: text/html; charset=utf-8\r\n";
+	if(mail("tkachenko.work@mail.ru", $subject, $message, $headers))
+		echo "Сообщение отправлено!";
+	else
+		echo "Сообщение не отправлено."; 
+?>
